c77b027b042348f96eda16cedb726109b0c49980
coconutpi <rsongkuntham@wesleyan.edu>
2017-03-30 01:52:15 -0400

    added timer, update readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WordChallenge
+
+Anagram game inspired by Playfish’s Word Challenge that doesn’t exist anymore
+
+## Status
+
+(no-frills version) The game is playable but some CSS and extra game features still need to be added.
+
+## Tool Used
+
+JavaScript, jQuery, HTML, CSS
+
+## Acknowledgement
+
+Thank you [NASPA Zyzzyva](https://github.com/boshvark/zyzzyva-pc) for providing dictionaries of valid words to Scrabble lovers all around the world. Zyzzyva was used at every Scrabble tournament I have gone to. I am excited to be able to use part of Zyzzyva open source for my Scrabble-like game project.
--- word.js
+++ word.js
@@ -1,9 +1,12 @@
 //handle start again (when hitting enter becomes start)
 //weighted tiles
+//timer
 var tiles = Array.from(document.getElementsByClassName("tile"));
 var played = Array.from(document.getElementsByClassName("played"));
 var score = document.getElementById("score");
 var error = document.getElementById("error");
+var timer = document.getElementById("timer");
+
 var vowels = [65,69,73,79,85]
 var consonants = [66,67,68,70,71,72,74,75,76,77,78,80,81,82,83,84,86,87,88,89,90];
 var originalRack;
@@ -16,6 +19,14 @@ $(function(){
             dict = data.split('\n');
         });
     });
+
+var original = 5; //change original value here
+var secDuration = original;	// How long the timer is set, in seconds
+var running = false;		// A boolean
+var timerInterval = secDuration;
+var clickStart = 0;
+var timesUp = false;
+
 function start(e) {
     //vowelsNum is randomized between 2 and 3
     var vowelsNum = Math.floor(Math.random() * 2) + 2;
@@ -30,9 +41,29 @@ function start(e) {
       tiles[index].innerHTML = String.fromCharCode(tile);  
     })
     originalRack = rack.slice();
+    
+    //timer
+    if (clickStart == 0) {
+        clickStart++;
+    }
+    else {
+        return;
+    }
+    timerInterval = setInterval(function() {
+        if (secDuration == 0) {
+            timesUp = true;
+            error.innerHTML = "Time's up!"
+        } else {
+            render(timer, (secDuration--) - 1);
+        }
+    }, 1000);
+    e.stopImmediatePropagation();
 }
 
 function play(e) {
+    if (timesUp) {
+        return;
+    }
     var key = e.keyCode
     if (key == 13) {
         played.forEach(function(each) {
@@ -64,5 +95,30 @@ function play(e) {
     
 }
 
+/*
+	getTimeString takes in a number of seconds and return
+	a time string in the format "mm:ss"
+	(2 digits for minutes, 2 digits for seconds)
+
+	Example: 	getTimeString(10) => "00:10"
+*/
+function getTimeString(totalSeconds) {
+	var ms = totalSeconds/60;
+    var s = Math.floor(totalSeconds%60);
+    var m = Math.floor(ms%60);
+    var second = s%10==s ? "0" + s : s;
+    var minute = m%10==m ? "0" + m : m;
+    return minute + ":" + second;
+}
+
+/*
+	render(displayDiv, totalSeconds) displays the totalSeconds
+	in "mm:ss" format in the browser where displayDiv is
+*/
+function render(displayDiv, totalSeconds) {
+	timer.innerHTML = getTimeString(totalSeconds);
+}
+
+render(timer, secDuration);
 document.getElementById("start").addEventListener("click", start);
 window.addEventListener('keydown', play);
